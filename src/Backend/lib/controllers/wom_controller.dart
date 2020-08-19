c628270f9d4992ff70680448b490c38ac2d60749
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2020-08-19 15:12:43 +0200

    Fix wrong womLink parameter

--- src/Backend/lib/controllers/wom_controller.dart
+++ src/Backend/lib/controllers/wom_controller.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:aqueduct/aqueduct.dart';
 import 'package:mirabilandia/api/wom_request_repo.dart';
+import 'package:mirabilandia/channel.dart';
 import 'package:mirabilandia/models/client_request.dart';
 
 class WomController extends ResourceController {
@@ -24,7 +24,7 @@ class WomController extends ResourceController {
     final verificationResponse =
     await _womRepository.verifyWomCreation(response);
     if (verificationResponse) {
-      return Response.ok({'womLink':response.otc,'womPassword':response.password,'womCount':30});
+      return Response.ok({'id':clientRequest.id,'womLink':'https://$womDomain/vouchers/${response.otc}' ,'womPassword':response.password,'womCount':30,});
     } else {
       return Response.serverError(body: {'error':'La verifica è fallita'});
     }
