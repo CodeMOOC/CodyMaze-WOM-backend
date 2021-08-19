89c7ff067279956c885c39193be7c78eb68cd211
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2021-08-19 12:48:45 +0200

    Removed aqueduct Migrate to null-safety

--- src/Backend/README.md
+++ src/Backend/README.md
@@ -1,21 +1,7 @@
-# mirabilandia
+## Build docker
 
-## Running the Application Locally
+`docker build -t codymaze:latest .`
 
-Run `aqueduct serve` from this directory to run the application. For running within an IDE, run `bin/main.dart`. By default, a configuration file named `config.yaml` will be used.
+## Launch
 
-To generate a SwaggerUI client, run `aqueduct document client`.
-
-## Running Application Tests
-
-To run all tests for this application, run the following in this directory:
-
-```
-pub run test
-```
-
-The default configuration file used when testing is `config.src.yaml`. This file should be checked into version control. It also the template for configuration files used in deployment.
-
-## Deploying an Application
-
-See the documentation for [Deployment](https://aqueduct.io/docs/deploy/).
+`docker run --rm -p 127.0.0.1:8080:8080 -e WOM_DOMAIN=wom.social -e SOURCE_ID=5f3ab6d898e66631aaeb60f2 -v "/private:/private" codymaze:latest`
--- src/Backend/bin/main.dart
+++ src/Backend/bin/main.dart
@@ -1,13 +1,108 @@
-import 'package:mirabilandia/mirabilandia.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
-Future main() async {
-  final app = Application<MirabilandiaChannel>()
-      ..options.configurationFilePath = "config.yaml"
-      ..options.port = 8888;
+import 'package:dart_wom_connector/dart_wom_connector.dart';
+import 'package:http/http.dart' as http;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart';
 
-  final count = Platform.numberOfProcessors ~/ 2;
-  await app.start(numberOfInstances: count > 0 ? count : 1);
+late String womDomain;
+late String sourceId;
+late String registryPublicKey;
+late String instrumentPrivateKey;
 
-  print("Application started on port: ${app.options.port}.");
-  print("Use Ctrl-C (SIGINT) to stop running the application.");
+const _hostname = '0.0.0.0';
+
+class Service {
+  final InstrumentClient instrumentClient;
+
+  Service(this.instrumentClient);
+
+  // The [Router] can be used to create a handler, which can be used with
+  // [shelf_io.serve].
+  Handler get handler {
+    final router = Router();
+
+    // Handlers can be added with `router.<verb>('<route>', handler)`, the
+    // '<route>' may embed URL-parameters, and these may be taken as parameters
+    // by the handler (but either all URL parameters or no URL parameters, must
+    // be taken parameters by the handler).
+    router.post('/vouchers', (Request request) async {
+      final map =
+          jsonDecode(await request.readAsString()) as Map<String, dynamic>;
+      final id = map['id'] as String;
+      final vouchers = <Voucher>[
+        Voucher(
+            lat: 43.7292635,
+            long: 12.6129107,
+            aim: 'E',
+            count: 30,
+            dateTime: DateTime.now()),
+      ];
+      try {
+        final response = await instrumentClient.requestVouchers(vouchers);
+
+        final verificationResponse =
+            await instrumentClient.verifyRequest(response);
+        if (verificationResponse) {
+          return Response.ok(jsonEncode({
+            'id': id,
+            'womLink': response.link,
+            'womPassword': response.password,
+            'womCount': 30,
+          }));
+        } else {
+          return Response.internalServerError(
+              body: {'error': 'La verifica è fallita'});
+        }
+      } catch (ex) {
+        return Response.internalServerError(body: {'error': ex.toString()});
+      }
+    });
+
+    router.get('/', (Request request) async {
+      await Future.delayed(const Duration(seconds: 1));
+      return Response.ok("We are live!");
+    });
+
+    return router;
+  }
+}
+
+Future<String> getRegistryPublicKey() async {
+  final registryPublicKeyResponse =
+      await http.get(Uri.parse('https://$womDomain/api/v2/auth/key'));
+  return registryPublicKeyResponse.body;
+}
+
+void main() async {
+  final envVars = Platform.environment;
+  womDomain = envVars['WOM_DOMAIN'] as String;
+  sourceId = envVars['SOURCE_ID'] as String;
+
+  registryPublicKey = await getRegistryPublicKey();
+
+  final privateKeyFile = File('/private/codymaze.pem');
+  instrumentPrivateKey = await privateKeyFile.readAsString();
+
+  final service = Service(InstrumentClient(
+      Instrument(
+        id: sourceId,
+        locationIsFixed: true,
+        defaultLocation: Location(
+          latitude: 43.7292635,
+          longitude: 12.6129107,
+        ),
+        name: 'CodyMaze',
+        privateKey: instrumentPrivateKey,
+        publicKey: '',
+        enabledAims: {'E'},
+        perAimBudget: {},
+      ),
+      womDomain,
+      registryPublicKey));
+  final server = await shelf_io.serve(service.handler, _hostname, 8080);
+  print('Serving at http://${server.address.host}:${server.port}');
 }
